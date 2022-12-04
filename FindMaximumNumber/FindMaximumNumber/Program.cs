7e9977dc0303c3cd78cf5a3feb10605845738c89
Nepolean23 <nlean1996@gmail.com>
2022-12-04 08:24:31 +0530

    Code To Find Maximum Strings

--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -1,7 +1,10 @@
 ﻿using FindMaximumNumber;
-///MaximumIntengersNumbers
+//*MaximumIntengersNumbers
 //int intoutput=UC_1.MaximumIntengersNumbers(1,2,3);
 //Console.WriteLine(intoutput);
-//MaximumFloatNumbers
-double doubleoutput=UC_2.MaximumNumbers(1.1,2.1,3.1);
-Console.WriteLine(doubleoutput);
+//*MaximumFloatNumbers
+//double doubleoutput=UC_2.MaximumNumbers(1.1,2.1,3.1);
+//Console.WriteLine(doubleoutput);
+//*MaximumStringNumbers
+string stringoutput = UC_3.MaximumStringNumbers("Apple", "Peach", "Bananna");
+Console.WriteLine(stringoutput);
---
+++ FindMaximumNumber/FindMaximumNumber/UC_3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class UC_3
+    {
+        public static string MaximumStringNumbers(string firstString,string secondString,string thirdString)
+        {
+            if(firstString.CompareTo(secondString)>0 && firstString.CompareTo(thirdString)>0 ||
+              firstString.CompareTo(secondString)>=0 && firstString.CompareTo(thirdString)>0 ||
+              firstString.CompareTo(secondString)>0 && firstString.CompareTo(thirdString)>=0)
+            {
+                return firstString;
+            }
+            if(secondString.CompareTo(thirdString) > 0 && secondString.CompareTo(firstString) > 0 ||
+              secondString.CompareTo(thirdString) >= 0 && secondString.CompareTo(firstString) > 0 ||
+              secondString.CompareTo(thirdString) > 0 && secondString.CompareTo(firstString) >= 0)
+            {
+                return secondString;
+            }
+            if(thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+              thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+              thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("firstString,secondString,thirdString are the same");
+        }
+    }
+}
