d730f589dc5826928fc72abdb564922d50134649
Nepolean23 <nlean1996@gmail.com>
2022-12-04 10:04:41 +0530

    Refactor Of Code By Using Generic Class

--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -11,7 +11,14 @@ using System.Xml;//*MaximumIntengersNumbers
 //*Generics methods
 //int  intoutput=Refactor1.MaximumNumbers<int>(1,2,3);
 //Console.WriteLine(intoutput);
-//double doubleoutput = Refactor1.MaximumNumbers<double>(1.1, 2.1, 3.1);
+//double doubleoutput = Refactor1.MaximumNumbers<double>(187, 195, 298);
 //Console.WriteLine(doubleoutput);
-//string stringoutput = Refactor1.MaximumNumbers<string>("Apple", "Peach", "Bananna");
+//string stringoutput = Refactor1.MaximumNumbers<string>("Tiger", "Lion", "Elephant");
 //Console.WriteLine(stringoutput);
+//*Generics class
+//int intoutput=Refactor2<int>.MaximumNumbers(100,200,300);
+//Console.WriteLine(intoutput);
+//double doubleoutput = Refactor2<double>.MaximumNumbers(35.9, 59.4, 98.5);
+//Console.WriteLine(doubleoutput);
+string stringoutput = Refactor2<string>.MaximumNumbers("Pen", "Scale", "Sharpner");
+Console.WriteLine(stringoutput);
